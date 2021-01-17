d73dd0d8bb263904cdf0e9c3ddf517cb4de8e1cc
fuyangfff <fuyang@bjdvt.com>
2021-01-17 12:59:14 +0800

    plan

---
+++ addons/plan/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Accountant',
+    'version': '1.0',
+    'category': '',
+    'depends': ['purchase'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/accountant_views.xml',
+    ],
+}
