a947c4c55d06886dfcf4100c112b3db27e89d713
fuyangfff <fuyang@bjdvt.com>
2021-01-18 09:18:37 +0800

    合并模块2

--- addons/accountant/models/res_parter.py
+++ addons/accountant/models/res_parter.py
@@ -39,84 +39,84 @@ class PurchaseOrderInherit(models.Model):
     def button_sure(self):
         self.write({'state': 'sure'})
 
-class SaleOrderInherit(models.Model):
-    _inherit = "sale.order"
+# class SaleOrderInherit(models.Model):
+#     _inherit = "sale.order"
+#
+#
+#
+#
+#
+#     @api.onchange('requisition_id')
+#     def _onchange_requisit_id(self):
+#         if not self.requisition_id:
+#             return
+#
+#         self = self.with_company(self.company_id)
+#         requisition = self.requisition_id
+#         if self.partner_id:
+#             partner = self.partner_id
+#         else:
+#             partner = requisition.vendor_id
+#         payment_term = partner.property_supplier_payment_term_id
+#
+#         FiscalPosition = self.env['account.fiscal.position']
+#         fpos = FiscalPosition.with_company(self.company_id).get_fiscal_position(partner.id)
+#
+#         self.partner_id = partner.id
+#         self.fiscal_position_id = fpos.id
+#         self.payment_term_id = payment_term.id,
+#         self.company_id = requisition.company_id.id
+#         self.currency_id = requisition.currency_id.id
+#         if not self.origin or requisition.name not in self.origin.split(', '):
+#             if self.origin:
+#                 if requisition.name:
+#                     self.origin = self.origin + ', ' + requisition.name
+#             else:
+#                 self.origin = requisition.name
+#         # self.notes = requisition.description
+#         self.date_order = fields.Datetime.now()
+#
+#         if requisition.type_id.line_copy != 'copy':
+#             return
+#
+#         # Create PO lines if necessary
+#         order_lines = []
+#         for line in requisition.line_ids:
+#             # Compute name
+#             product_lang = line.product_id.with_context(
+#                 lang=partner.lang,
+#                 partner_id=partner.id
+#             )
+#             name = product_lang.display_name
+#             if product_lang.description_purchase:
+#                 name += '\n' + product_lang.description_purchase
+#
+#             # Compute taxes
+#             taxes_ids = fpos.map_tax(
+#                 line.product_id.supplier_taxes_id.filtered(lambda tax: tax.company_id == requisition.company_id)).ids
+#
+#             # Compute quantity and price_unit
+#             if line.product_uom_id != line.product_id.uom_po_id:
+#                 product_qty = line.product_uom_id._compute_quantity(line.product_qty, line.product_id.uom_po_id)
+#                 price_unit = line.product_uom_id._compute_price(line.price_unit, line.product_id.uom_po_id)
+#             else:
+#                 product_qty = line.product_qty
+#                 price_unit = line.price_unit
+#
+#             if requisition.type_id.quantity_copy != 'copy':
+#                 product_qty = 0
+#
+#             # Create PO line
+#             order_line_values = {
+#                 'name': name, 'product_uom_qty': product_qty, 'price_unit': price_unit,
+#                 'tax_id': taxes_ids, 'product_id': line.product_id.id, 'product_uom': line.product_id.uom_po_id.id}
+#             order_lines.append((0, 0, order_line_values))
+#         self.order_line = order_lines
 
-
-
-
-
-    @api.onchange('requisition_id')
-    def _onchange_requisit_id(self):
-        if not self.requisition_id:
-            return
-
-        self = self.with_company(self.company_id)
-        requisition = self.requisition_id
-        if self.partner_id:
-            partner = self.partner_id
-        else:
-            partner = requisition.vendor_id
-        payment_term = partner.property_supplier_payment_term_id
-
-        FiscalPosition = self.env['account.fiscal.position']
-        fpos = FiscalPosition.with_company(self.company_id).get_fiscal_position(partner.id)
-
-        self.partner_id = partner.id
-        self.fiscal_position_id = fpos.id
-        self.payment_term_id = payment_term.id,
-        self.company_id = requisition.company_id.id
-        self.currency_id = requisition.currency_id.id
-        if not self.origin or requisition.name not in self.origin.split(', '):
-            if self.origin:
-                if requisition.name:
-                    self.origin = self.origin + ', ' + requisition.name
-            else:
-                self.origin = requisition.name
-        # self.notes = requisition.description
-        self.date_order = fields.Datetime.now()
-
-        if requisition.type_id.line_copy != 'copy':
-            return
-
-        # Create PO lines if necessary
-        order_lines = []
-        for line in requisition.line_ids:
-            # Compute name
-            product_lang = line.product_id.with_context(
-                lang=partner.lang,
-                partner_id=partner.id
-            )
-            name = product_lang.display_name
-            if product_lang.description_purchase:
-                name += '\n' + product_lang.description_purchase
-
-            # Compute taxes
-            taxes_ids = fpos.map_tax(
-                line.product_id.supplier_taxes_id.filtered(lambda tax: tax.company_id == requisition.company_id)).ids
-
-            # Compute quantity and price_unit
-            if line.product_uom_id != line.product_id.uom_po_id:
-                product_qty = line.product_uom_id._compute_quantity(line.product_qty, line.product_id.uom_po_id)
-                price_unit = line.product_uom_id._compute_price(line.price_unit, line.product_id.uom_po_id)
-            else:
-                product_qty = line.product_qty
-                price_unit = line.price_unit
-
-            if requisition.type_id.quantity_copy != 'copy':
-                product_qty = 0
-
-            # Create PO line
-            order_line_values = {
-                'name': name, 'product_uom_qty': product_qty, 'price_unit': price_unit,
-                'tax_id': taxes_ids, 'product_id': line.product_id.id, 'product_uom': line.product_id.uom_po_id.id}
-            order_lines.append((0, 0, order_line_values))
-        self.order_line = order_lines
-
-class SaleOrderLineInherit(models.Model):
-    _inherit = "sale.order.line"
-
-    customer_lead = fields.Float(
-        'Lead Time', default=0.0,
-        help="Number of days between the order confirmation and the shipping of the products to the customer")
+# class SaleOrderLineInherit(models.Model):
+#     _inherit = "sale.order.line"
+#
+#     customer_lead = fields.Float(
+#         'Lead Time', default=0.0,
+#         help="Number of days between the order confirmation and the shipping of the products to the customer")
 
